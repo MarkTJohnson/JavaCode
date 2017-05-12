8586d7ba7dc423820eb4015091e5a8c6898e4397
Mark T Johnson <MarkTravis.Johnson@Concur.com>
2017-05-12 17:43:24 -0400

    Stash test two
    Made a change to the file that was stashed in the main branch

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -2,8 +2,11 @@ public class PhraseOMatic
 {
 	public static void main(String[] args)
 	{
-		String[] wordListOne = {"24/7", "multi-tier", "30,000 foot", "win-win", "front-end", "web-based", "pervasive",       									"smart", "six-sigma", "critical-path", "dynamic"};
-		String[] wordListTwo = {"empowered", "sitcky", "value-added", " oriented", "centric", "distributed", "clusterd", 									"branded", "outside-the-box", "positioned", "networked", "focused", "leveraged", "aligned", 								"targeted", "shared", "cooperative", "accelerated"};
+		String[] wordListOne = {"24/7", "multi-tier", "30,000 foot", "win-win", "front-end", "web-based", "pervasive",       									
+		                        "smart", "six-sigma", "critical-path", "dynamic"};
+		String[] wordListTwo = {"empowered", "sitcky", "value-added", " oriented", "centric", "distributed", "clusterd",
+		                        "branded", "outside-the-box", "positioned", "networked", "focused", "leveraged", "aligned", 								
+		                        "targeted", "shared", "cooperative", "accelerated"};
         String[] wordListThree = {"process", "tipping-point", "solution", "architecture", "core competency",
                                   "strategy", "mindshare", "portal", "space", "vision", "paradigm", "mission"};
 
@@ -12,6 +15,6 @@ public class PhraseOMatic
         int rand3 = (int) (Math.random() * wordListThree.length);
 
         String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
-        System.out.println("What we need is a " + phrase);
+        System.out.println("What we need is a " + phrase + " and we need it right now!");
 	}
 }
